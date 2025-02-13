f5963066e0bc9c67c9649a33687620a57c24b280
Lucas Lean Nascimento <kashchannel@gmail.com>
2025-02-12 23:17:17 -0300

    Update index.js

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,6 @@
 import app from 'flarum/admin/app';
 
-app.initializers.add('fof/discussion-thumbnail', () => {
+app.initializers.add('leanzo/discussion-thumbnail', () => {
     app.extensionData.for('fof-discussion-thumbnail').registerSetting({
         label: app.translator.trans('fof-discussion-thumbnail.admin.settings.link_to_discussion_label'),
         setting: 'fof-discussion-thumbnail.link_to_discussion',
